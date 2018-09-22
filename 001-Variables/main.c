901b15fa1d10f64e6cc9f96e844ebb725dfd00fe
luis <yongchun1683@gmail.com>
2018-09-22 22:01:29 +0800

    add variables

---
+++ 001-Variables/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char username[] = "John";
+    int userage = 20;
+
+    printf("There once was a man named %s.\n", username);
+    printf("He was %d years old.\n", userage);
+}
